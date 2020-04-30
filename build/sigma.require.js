b393482d22ba263f9f6b5f36aeebb14a1eeccfd1
Joey Yang <zeyuyang2022@u.northwestern.edu>
2020-04-30 01:54:32 -0400

    adjusted

--- build/sigma.require.js
+++ build/sigma.require.js
@@ -9690,7 +9690,7 @@ sigma.canvas.labels.def = function(node, context, settings) {
   labelWidth = context.measureText('W').width * label.length + label.length / 2;
   charWidth = context.measureText('W').width;
   if (!node.direction || node.direction === 'up'){
-    labelPlacementY = Math.round(node[prefix + 'y'] + fontSize - 4) - size - labelWidth;
+    labelPlacementY = Math.round(node[prefix + 'y'] + fontSize - 1) - size - labelWidth;
   } else {
     labelPlacementY = Math.round(node[prefix + 'y'] + fontSize / 3) + size + fontSize;
   }
